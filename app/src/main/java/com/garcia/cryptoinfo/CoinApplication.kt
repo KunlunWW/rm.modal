01ae50719ebdee7761a00c288ee569643b885be0
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2020-07-10 18:33:10 +0000

    drm/radeon: properly handle crtc powergating

--- app/src/main/java/com/garcia/cryptoinfo/CoinApplication.kt
+++ app/src/main/java/com/garcia/cryptoinfo/CoinApplication.kt
@@ -1,4 +1,7 @@
 package com.garcia.cryptoinfo
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroid
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinApplication: Application(
