7edea61888e3a1010b9c67e76a79815cbbad5ada
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2020-07-27 08:17:00 +0000

    Small comment fix.

--- app/src/main/java/com/garcia/cryptoinfo/common/Constants.kt
+++ app/src/main/java/com/garcia/cryptoinfo/common/Constants.kt
@@ -2,4 +2,4 @@ package com.garcia.cryptoinfo.common
 
 object Constants {
     const val BASE_URL = "https://api.coinpaprika.com/"
-    const val C
+    const val COIN_LOGO_BASE_URL = "https://static.coinpaprika.com/coin/
