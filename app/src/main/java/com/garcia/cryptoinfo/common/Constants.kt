827ecac9844779006475f57ac3a50072e344d540
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2020-08-09 22:14:30 +0000

    release: release version

--- app/src/main/java/com/garcia/cryptoinfo/common/Constants.kt
+++ app/src/main/java/com/garcia/cryptoinfo/common/Constants.kt
@@ -4,4 +4,4 @@ object Constants {
     const val BASE_URL = "https://api.coinpaprika.com/"
     const val COIN_LOGO_BASE_URL = "https://static.coinpaprika.com/coin/"
     const val LOGO_PNG = "/logo.png"
-    c
+    const val CHART_BASE_URL  = "https://graphs.coinpa
