a710142a7a421a0ac8bf964d75669f72bce3cf7b
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2020-08-09 13:34:32 +0000

    Added binary

--- app/src/main/java/com/garcia/cryptoinfo/common/Constants.kt
+++ app/src/main/java/com/garcia/cryptoinfo/common/Constants.kt
@@ -2,4 +2,6 @@ package com.garcia.cryptoinfo.common
 
 object Constants {
     const val BASE_URL = "https://api.coinpaprika.com/"
-    const val COIN_LOGO_BASE_URL = "https://static.coinpaprika.com/coin/
+    const val COIN_LOGO_BASE_URL = "https://static.coinpaprika.com/coin/"
+    const val LOGO_PNG = "/logo.png"
+    c
