31f921ed1005b2596d623ed1beada823a29ca228
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2020-08-10 08:39:59 +0000

    Project 0 2D v0.03:

--- app/src/main/java/com/garcia/cryptoinfo/common/Constants.kt
+++ app/src/main/java/com/garcia/cryptoinfo/common/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val COIN_LOGO_BASE_URL = "https://static.coinpaprika.com/coin/"
     const val LOGO_PNG = "/logo.png"
     const val CHART_BASE_URL  = "https://graphs.coinpaprika.com/currency/chart/"
-    const val CHART_7D
+    const val CHART_7DAYS = "/7d/chart.svg"
+}
---
+++ app/src/main/java/com/garcia/cryptoinfo/common/Error.kt
@@ -0,0 +1,9 @@
+
+package com.garcia.cryptoinfo.common
+
+import com.garcia.cryptoinfo.R
+
+data class Error(
+    val message: String? = null,
+    val resourceId: Int = R.string.generic_error,
+)
