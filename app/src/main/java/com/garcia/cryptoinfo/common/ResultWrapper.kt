c52630cf0c64e113c9614a1860b4814f966aa9de
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2020-08-10 18:18:48 +0000

    up deps

--- app/src/main/java/com/garcia/cryptoinfo/common/ResultWrapper.kt
+++ app/src/main/java/com/garcia/cryptoinfo/common/ResultWrapper.kt
@@ -1,4 +1,5 @@
 package com.garcia.cryptoinfo.common
 
 sealed class ResultWrapper<out T> {
-    data class Success<out T>(val value: T?) : Re
+    data class Success<out T>(val value: T?) : ResultWrapper<T>()
+    data class Error(val code: Int? = null, v
