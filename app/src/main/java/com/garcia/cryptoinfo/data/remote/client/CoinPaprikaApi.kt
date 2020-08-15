0acaf8862b352f27a1a0f6223632bb003aecd723
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2020-08-15 13:30:23 +0000

    Fixed indentation error

--- app/src/main/java/com/garcia/cryptoinfo/data/remote/client/CoinPaprikaApi.kt
+++ app/src/main/java/com/garcia/cryptoinfo/data/remote/client/CoinPaprikaApi.kt
@@ -3,4 +3,8 @@ package com.garcia.cryptoinfo.data.remote.client
 import com.garcia.cryptoinfo.data.remote.dto.CoinDetailDto
 import com.garcia.cryptoinfo.data.remote.dto.CoinDto
 import retrofit2.http.GET
-import retrofit2.http
+import retrofit2.http.Path
+
+interface CoinPaprikaApi {
+
+    @GET("v1/c
