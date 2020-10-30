9ae860cabdd2a07d6b09f3b682525cb552186fa4
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2020-10-30 09:29:01 +0000

    initial styling

--- app/src/main/java/com/garcia/cryptoinfo/data/remote/network/ResponseHandler.kt
+++ app/src/main/java/com/garcia/cryptoinfo/data/remote/network/ResponseHandler.kt
@@ -3,4 +3,7 @@ package com.garcia.cryptoinfo.data.remote.network
 import com.garcia.cryptoinfo.common.ResultWrapper
 import retrofit2.HttpException
 import java.io.IOException
-import javax.inject.In
+import javax.inject.Inject
+
+class ResponseHandler @Inject constructor() {
+
