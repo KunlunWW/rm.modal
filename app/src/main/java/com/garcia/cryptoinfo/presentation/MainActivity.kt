917d9e9d6a16d41f60e24f730c964c08042eeeb6
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2020-12-10 12:18:47 +0000

    Changed logging

--- app/src/main/java/com/garcia/cryptoinfo/presentation/MainActivity.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/MainActivity.kt
@@ -2,4 +2,5 @@ package com.garcia.cryptoinfo.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setC
+import androidx.activity.compose.setContent
+import androidx.compose.material.Ma
