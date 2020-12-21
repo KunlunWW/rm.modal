eebad74428ac02456af41b65ed2af8f3a65157f2
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2020-12-21 08:51:18 +0000

    merge refactoring of the bootstrap->chef cookbooks

--- app/src/main/java/com/garcia/cryptoinfo/presentation/MainActivity.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/MainActivity.kt
@@ -17,4 +17,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       
+        setContent {
+            cryptoInfoAppTheme {
+  
