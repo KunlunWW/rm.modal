a15514a94bff8adf6ccd15e88c67b2e1c51f85a7
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-01-30 14:41:09 +0000

    nilfs2: remove double bio_put() in nilfs_end_bio_write() for BIO_EOPNOTSUPP error

--- app/src/main/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailScreen.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailScreen.kt
@@ -2,4 +2,5 @@ package com.garcia.cryptoinfo.presentation.coin_detail
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundat
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundati
