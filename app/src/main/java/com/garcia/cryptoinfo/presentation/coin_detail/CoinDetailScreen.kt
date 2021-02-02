962a59135430ad91c60cd85f8f6e6500c65041a8
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-02-02 22:17:03 +0000

    upgrade modules

--- app/src/main/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailScreen.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailScreen.kt
@@ -8,4 +8,5 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import andr
