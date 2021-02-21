f9d81907618a07bf67d77c0da6cb653e8b1c607a
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-02-21 11:10:10 +0000

    first blog

--- app/src/main/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailScreen.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailScreen.kt
@@ -12,4 +12,5 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.pl
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.Fo
