10523f05a0836142d77b595a20f0983b05fa1ac0
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-03-01 21:52:19 +0000

    Make executable.

--- app/src/main/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailScreen.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailScreen.kt
@@ -16,4 +16,5 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.to
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.u
