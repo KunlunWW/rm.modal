b736e49b979eec830e03112399673339ad5a6bdd
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-03-06 22:16:39 +0000

    (no message)

--- app/src/main/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailScreen.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailScreen.kt
@@ -24,4 +24,5 @@ import coil.compose.rememberImagePainter
 import coil.decode.SvgDecoder
 import com.garcia.cryptoinfo.common.Constants
 import com.garcia.cryptoinfo.presentation.coin_detail.components.CoinTag
-import com.garcia.cryptoinfo.presentation.coin_detail.components.TeamList
+import com.garcia.cryptoinfo.presentation.coin_detail.components.TeamListItem
+import com.google.accompanist.flo
