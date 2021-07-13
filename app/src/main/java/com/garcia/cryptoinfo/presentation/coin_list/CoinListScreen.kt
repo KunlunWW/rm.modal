67112f8ce3e136cfa71a25642569d5b1a55af194
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-07-13 20:40:52 +0000

    Added initializer

--- app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListScreen.kt
@@ -3,4 +3,5 @@ package com.garcia.cryptoinfo.presentation.coin_list
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.
+import androidx.compose.foundation.layout.padding
+import androidx.compose.found
