05c4ca47debdb338a42ec3e1ce77fcf2dfb6d370
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-07-16 20:52:19 +0000

    updated build.xml

--- app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListScreen.kt
@@ -8,4 +8,5 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
-import an
+import androidx.compose.material.Text
+import androidx.co
