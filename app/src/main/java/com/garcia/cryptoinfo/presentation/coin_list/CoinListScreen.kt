e2ae586747a87a2e35c1511861b3134d27d1be19
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-07-16 05:36:05 +0000

    Changed ondemand video list into checked-list-box type.

--- app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListScreen.kt
@@ -5,4 +5,5 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.fo
+import androidx.compose.foundation.lazy.items
+import androidx.co
