ca605a75fbfa09a17390d8bbe3cea032f5a36ad6
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-08-08 23:04:35 +0000

    Delete Procfile

--- app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/components/CoinListItem.kt
@@ -2,4 +2,5 @@ package com.garcia.cryptoinfo.presentation.coin_list.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-im
+import androidx.compose.foundation.layout.*
+import androidx.c
