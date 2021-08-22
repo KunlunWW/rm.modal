3e7ec1e034d6bf16ab95737cea73de9cf2a7059d
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-08-22 10:05:40 +0000

    megaraid_sas: corrected return of wait_event from abort frame path

--- app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/components/CoinListItem.kt
@@ -8,4 +8,5 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui
+import androidx.compose.ui.layout.ContentScale
+import androidx.comp
