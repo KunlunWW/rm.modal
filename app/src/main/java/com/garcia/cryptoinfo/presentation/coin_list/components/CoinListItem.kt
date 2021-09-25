740a80d32bb17a99db9d4581802fd12746447f28
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-09-25 11:55:06 +0000

    drm/i915: Invalidate media caches on gen7

--- app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/coin_list/components/CoinListItem.kt
@@ -32,4 +32,8 @@ fun CoinListItem(
             .fillMaxWidth()
             .clickable { onItemClick(coin) }
             .padding(20.dp),
-        horizontalArrangement = Arrangement.SpaceBe
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Row(
+   
