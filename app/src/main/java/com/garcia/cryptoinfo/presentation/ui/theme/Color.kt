ae0e8541690c385c2062a7d32cf829d2e7fc7ea4
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-11-05 16:20:35 +0000

    fix dump

--- app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Color.kt
@@ -2,4 +2,6 @@ package com.garcia.cryptoinfo.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val DarkGray
+val DarkGray = Color(0xFF202020)
+val MediumGray = Color(0xFF505050)
+va
