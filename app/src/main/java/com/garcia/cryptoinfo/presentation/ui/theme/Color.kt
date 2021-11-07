7d0554e74c931a321cfd8ed37df7fd45d3119ca1
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-11-07 16:09:51 +0000

    more posts

--- app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Color.kt
@@ -5,4 +5,4 @@ import androidx.compose.ui.graphics.Color
 val DarkGray = Color(0xFF202020)
 val MediumGray = Color(0xFF505050)
 val ColorPrimary = Color(0xFF08FF04)
-val TextWhite = Colo
+val TextWhite = Color(0xFFEEEEEE)
