03930a9a6431315c7ca6f5bcdc4009bc4906314d
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-11-13 10:36:36 +0000

    Removed extra files

--- app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Shape.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Shape.kt
@@ -1,3 +1,5 @@
 package com.garcia.cryptoinfo.presentation.ui.theme
 
-import androidx.compose.foundation.shape.RoundedCornerS
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+imp
