f4ff3ef9c8803235d9f10244d8d074043ea5eeab
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-11-21 18:51:32 +0000

    Minor bug fixing

--- app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Shape.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Shape.kt
@@ -7,4 +7,5 @@ import androidx.compose.ui.unit.dp
 val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
-    large = Roun
+    large = RoundedCornerShape(0.dp)
+)
