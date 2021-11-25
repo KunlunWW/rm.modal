80a340b72c07eeeaf4d6e0f911d000ed6bfdc2de
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-11-25 21:42:10 +0000

    мелочи

--- app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Theme.kt
@@ -3,4 +3,5 @@ package com.garcia.cryptoinfo.presentation.ui.theme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import andro
+import androidx.compose.runtime.Composable
+import androidx.compo
