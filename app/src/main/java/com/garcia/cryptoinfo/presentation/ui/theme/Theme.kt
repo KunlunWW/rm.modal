e1565316000e6a84a20599342dbec1dabe624f57
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-11-25 11:34:19 +0000

    Add initial docs

--- app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Theme.kt
@@ -2,4 +2,5 @@ package com.garcia.cryptoinfo.presentation.ui.theme
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-impor
+import androidx.compose.material.lightColors
+import andro
