06d31c64135b44ae6f7b73f8565d5e4c01940db4
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-12-12 18:34:33 +0000

    rspec --init

--- app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Type.kt
@@ -3,4 +3,5 @@ package com.garcia.cryptoinfo.presentation.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.te
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit
