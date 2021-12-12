c90756c35ed6ff8bde8faa8b7baa4d22a36441b8
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-12-12 14:29:55 +0000

    [de] add words suggested by users

--- app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Type.kt
@@ -2,4 +2,5 @@ package com.garcia.cryptoinfo.presentation.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.te
