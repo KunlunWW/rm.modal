6d0a88c172de60fe3025f32834b6b7e50fde1c81
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-12-12 12:37:29 +0000

    import files

--- app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Type.kt
@@ -1,4 +1,5 @@
 package com.garcia.cryptoinfo.presentation.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextSt
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font
