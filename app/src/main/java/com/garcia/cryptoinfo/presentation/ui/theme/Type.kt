b296a925124d647e2c7b79399724a18f8b3db9e1
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2021-12-19 08:11:34 +0000

    added travis integration

--- app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/garcia/cryptoinfo/presentation/ui/theme/Type.kt
@@ -6,4 +6,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography style
+// Set of Material typography styles to start with
+val Typography = Typography(
+    
