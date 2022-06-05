edc01afb70b503bfe44a36f3414dd2f92460908d
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-06-05 20:36:37 +0000

    bump atc fly

--- app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coin/GetCoinUseCaseTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coin/GetCoinUseCaseTest.kt
@@ -2,4 +2,5 @@ package com.garcia.cryptoinfo.domain.use_case.get_coin
 
 import com.garcia.cryptoinfo.common.ResultWrapper
 import com.garcia.cryptoinfo.domain.utils.DomainObjectsMocks
+import com.garcia.cryptoinfo.domain.model.CoinDetail
 i
