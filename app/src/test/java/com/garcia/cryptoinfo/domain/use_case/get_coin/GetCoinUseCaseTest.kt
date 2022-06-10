3802ffe3d0352463b84dd359bd803ba3bbaf324e
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-06-10 20:02:13 +0000

    Prepare 0.6.0 release

--- app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coin/GetCoinUseCaseTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coin/GetCoinUseCaseTest.kt
@@ -3,4 +3,5 @@ package com.garcia.cryptoinfo.domain.use_case.get_coin
 import com.garcia.cryptoinfo.common.ResultWrapper
 import com.garcia.cryptoinfo.domain.utils.DomainObjectsMocks
 import com.garcia.cryptoinfo.domain.model.CoinDetail
-import com.garcia.cryptoinfo.domain.reposi
+import com.garcia.cryptoinfo.domain.repository.CoinRepository
+import io.mockk.MockKAnnotation
