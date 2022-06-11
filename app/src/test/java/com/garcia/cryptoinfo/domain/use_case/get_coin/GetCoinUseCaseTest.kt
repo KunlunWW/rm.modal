1357d796a8633c5b9600bd04fb066c6390a036df
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-06-11 22:04:33 +0000

    Added translation using Weblate (Croatian)

--- app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coin/GetCoinUseCaseTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coin/GetCoinUseCaseTest.kt
@@ -4,4 +4,6 @@ import com.garcia.cryptoinfo.common.ResultWrapper
 import com.garcia.cryptoinfo.domain.utils.DomainObjectsMocks
 import com.garcia.cryptoinfo.domain.model.CoinDetail
 import com.garcia.cryptoinfo.domain.repository.CoinRepository
-import io.mockk.MockKAnnotation
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.a
