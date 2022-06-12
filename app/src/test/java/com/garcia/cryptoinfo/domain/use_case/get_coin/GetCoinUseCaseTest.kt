7130ce933971a5815452fba8ac22a7b92f89fcca
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-06-12 14:02:53 +0000

    Update D2.1

--- app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coin/GetCoinUseCaseTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coin/GetCoinUseCaseTest.kt
@@ -7,4 +7,5 @@ import com.garcia.cryptoinfo.domain.repository.CoinRepository
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
-import kotlinx.coroutines.Experi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow
