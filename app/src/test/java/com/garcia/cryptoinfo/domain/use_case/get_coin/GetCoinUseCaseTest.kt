34d30ad6648b5d7f13bf2b249efd79a8e6303256
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-08-25 22:15:07 +0000

    add heroku

--- app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coin/GetCoinUseCaseTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coin/GetCoinUseCaseTest.kt
@@ -84,4 +84,5 @@ class GetCoinUseCaseTest{
         result.first() shouldBeEqualTo ResultWrapper.NetworkError
     }
 
-    private fun gi
+    private fun given(
+        result: ResultWrapper<CoinDetail
