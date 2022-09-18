7119b50d7ca93eb3220e80ac379e62053410f6a7
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-09-18 15:14:08 +0000

    Ignore config file

--- app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coins/GetCoinsUseCaseTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coins/GetCoinsUseCaseTest.kt
@@ -3,4 +3,5 @@ package com.garcia.cryptoinfo.domain.use_case.get_coins
 import com.garcia.cryptoinfo.common.ResultWrapper
 import com.garcia.cryptoinfo.domain.utils.DomainObjectsMocks
 import com.garcia.cryptoinfo.domain.model.Coin
-import com.garcia.cryptoinfo
+import com.garcia.cryptoinfo.domain.repository.CoinRepository
+import io.moc
