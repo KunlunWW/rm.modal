6e78e8c4a3a4d165412ad78428119430da980593
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-09-18 08:54:12 +0000

    Mateo

--- app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coins/GetCoinsUseCaseTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coins/GetCoinsUseCaseTest.kt
@@ -2,4 +2,5 @@ package com.garcia.cryptoinfo.domain.use_case.get_coins
 
 import com.garcia.cryptoinfo.common.ResultWrapper
 import com.garcia.cryptoinfo.domain.utils.DomainObjectsMocks
-import com.garcia.cry
+import com.garcia.cryptoinfo.domain.model.Coin
+import com.garcia.cryptoinfo
