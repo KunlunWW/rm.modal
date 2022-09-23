d7cc711a17669c6bc0ad0453dc77f39c3b237454
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-09-23 09:56:52 +0000

    Trying to fix tests

--- app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coins/GetCoinsUseCaseTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coins/GetCoinsUseCaseTest.kt
@@ -4,4 +4,6 @@ import com.garcia.cryptoinfo.common.ResultWrapper
 import com.garcia.cryptoinfo.domain.utils.DomainObjectsMocks
 import com.garcia.cryptoinfo.domain.model.Coin
 import com.garcia.cryptoinfo.domain.repository.CoinRepository
-import io.moc
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.m
