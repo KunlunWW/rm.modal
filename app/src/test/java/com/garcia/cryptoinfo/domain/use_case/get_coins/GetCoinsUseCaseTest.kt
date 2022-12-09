152767442ad4e7aed84a341035ff630a92d7cbc0
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-12-09 08:02:07 +0000

    Update authentication.md

--- app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coins/GetCoinsUseCaseTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/domain/use_case/get_coins/GetCoinsUseCaseTest.kt
@@ -73,4 +73,4 @@ class GetCoinsUseCaseTest{
 
     @ExperimentalCoroutinesApi
     @Test
-    fun `should return ResultWrapperNetworkError 
+    fun `should return ResultWrapperNetworkError when getCoins returns networkError`() = r
