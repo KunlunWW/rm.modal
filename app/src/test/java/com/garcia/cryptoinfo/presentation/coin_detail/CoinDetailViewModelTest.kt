c386392c69ec8431d99b0c39514315e609a0273d
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-12-20 18:43:24 +0000

    init import

--- app/src/test/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailViewModelTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/presentation/coin_detail/CoinDetailViewModelTest.kt
@@ -15,3 +15,52 @@ import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import org.amshove.kluent.shouldBeEqualTo
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@ExperimentalCoroutinesApi
+@RunWith(JUnit4::class)
+class CoinDetailViewModelTest{
+
+    @get:Rule
+    var coroutinesTestRule = CoroutineRule()
+
+    private lateinit var viewModel: CoinDetailViewModel
+
+    @MockK(relaxed = true)
+    lateinit var getCoinUseCase: GetCoinUseCase
+
+    lateinit var savedStateHandle: SavedStateHandle
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        savedStateHandle = SavedStateHandle()
+        savedStateHandle.set("coinId","123")
+    }
+
+    @Test
+    fun `verify state when getCoinUseCase returns Success`() {
+        // given
+        val data = DomainObjectsMocks.getCoinDetail("10")
+        given(ResultWrapper.Success(data))
+
+        // when
+        viewModel = CoinDetailViewModel(getCoinUseCase, savedStateHandle)
+
+        // verify
+        viewModel.state.value shouldBeEqualTo CoinDetailViewModel.ViewState(
+            isLoading = false,
+            coin = data,
+            error = null
+        )
+    }
+
+    @Test
+    fun `verify state when getCoinUseCase returns Error`() {
+        // given
+        val error = ResultWrapper.Error(message = "Error message")
+        given(error)
