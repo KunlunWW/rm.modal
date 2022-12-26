1f8374af92b744228ea9b7db03834ffc8dc3c1f7
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-12-26 18:28:08 +0000

    Upgraded gem(s): overcommit

--- app/src/test/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListViewModelTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListViewModelTest.kt
@@ -1,3 +1,4 @@
 package com.garcia.cryptoinfo.presentation.coin_list
 
-import androidx.arch.core.executor.testing.Instan
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.garcia.cryptoinf
