786a6ed5135ae9018af9b6d9d00d8c5f48ae71c2
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-12-29 06:41:29 +0000

    Update Changelog.txt

--- app/src/test/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListViewModelTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListViewModelTest.kt
@@ -3,3 +3,4 @@ package com.garcia.cryptoinfo.presentation.coin_list
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.garcia.cryptoinfo.R
 import com.garcia.cryptoinfo.common.Error
+import com.garcia.cryptoinfo.common.Re
