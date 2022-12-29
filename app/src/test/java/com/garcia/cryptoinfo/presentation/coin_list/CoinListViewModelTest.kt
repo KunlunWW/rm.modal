358219869f26c57f468775fe1738391263d6781b
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2022-12-29 15:28:27 +0000

    improved demo

--- app/src/test/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListViewModelTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListViewModelTest.kt
@@ -4,4 +4,5 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.garcia.cryptoinfo.R
 import com.garcia.cryptoinfo.common.Error
 import com.garcia.cryptoinfo.common.ResultWrapper
-import com.garcia.cryptoinfo.domai
+import com.garcia.cryptoinfo.domain.model.Coin
+import com.garcia.cryptoinfo.domain.
