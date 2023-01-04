2b27db00e2a2b842f6162ee24f6d0ce9118df7d6
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2023-01-04 15:25:50 +0000

    Bump dependency

--- app/src/test/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListViewModelTest.kt
+++ app/src/test/java/com/garcia/cryptoinfo/presentation/coin_list/CoinListViewModelTest.kt
@@ -8,4 +8,5 @@ import com.garcia.cryptoinfo.domain.model.Coin
 import com.garcia.cryptoinfo.domain.use_case.get_coins.GetCoinsUseCase
 import com.garcia.cryptoinfo.domain.utils.DomainObjectsMocks
 import com.garcia.cryptoinfo.utils.CoroutineRule
+import io.mockk.MockKAnnotations
 import
