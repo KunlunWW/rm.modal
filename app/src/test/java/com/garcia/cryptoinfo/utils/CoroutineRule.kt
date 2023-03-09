01f26b838c5c914cbcbfb398f0c337b52f342526
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2023-03-09 18:36:26 +0000

    Update pt-BR.json

--- app/src/test/java/com/garcia/cryptoinfo/utils/CoroutineRule.kt
+++ app/src/test/java/com/garcia/cryptoinfo/utils/CoroutineRule.kt
@@ -1,4 +1,5 @@
 package com.garcia.cryptoinfo.utils
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.test.TestCoroutineDispatch
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test
