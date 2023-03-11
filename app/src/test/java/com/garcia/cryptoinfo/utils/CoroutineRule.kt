3647d0e61c1e12520cfd1f064c65f56a43a934c6
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2023-03-11 12:34:41 +0000

    Rename fontbakery

--- app/src/test/java/com/garcia/cryptoinfo/utils/CoroutineRule.kt
+++ app/src/test/java/com/garcia/cryptoinfo/utils/CoroutineRule.kt
@@ -2,4 +2,6 @@ package com.garcia.cryptoinfo.utils
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+i
