e593a6023071a9a8b731b1b586675968563dd66a
KunlunWW <106032004+KunlunWW@users.noreply.github.com>
2023-03-13 07:35:33 +0000

    Update devDependencies.

--- app/src/test/java/com/garcia/cryptoinfo/utils/CoroutineRule.kt
+++ app/src/test/java/com/garcia/cryptoinfo/utils/CoroutineRule.kt
@@ -5,4 +5,6 @@ import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
-import or
+import org.junit.runner.Description
+
+class Coroutine
